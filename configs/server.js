7fc1ba3bd75b4038db671b6d0e3a6cfab234a424
Jeff <jportillo@kinal.edu.gt>
2025-02-25 23:49:08 -0600

    creacion base de datos

---
+++ configs/server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cors());
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan('dev'));
+}
+
+const routes = (app) => {
+
+}
+
+const conectarDB = async () => {
+    try{
+        await dbConnection();
+        console.log("Conexión a la base de datos exitosa");
+    }catch(error){
+        console.error('Error conectando a la base de datos', error);
+        process.exit(1);
+    }
+}
+
+export const iniciarServidor = async () => {
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    try {
+        middlewares(app);
+        conectarDB();
+        routes(app);
+        app.listen(port);
+        console.log(`Server running on port: ${port}`);
+    } catch (err) {
+        console.log(`Server init failed: ${err}`);
+    }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import {config} from 'dotenv';
+config();
+
+import { iniciarServidor } from './configs/server.js';
+
+iniciarServidor();
